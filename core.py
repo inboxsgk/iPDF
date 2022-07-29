7bdd76d4a971323beb58e7c3d0617efd64cb3a97
Gowtham Karthikeyan <30730201+inboxsgk@users.noreply.github.com>
2022-07-29 11:33:52 +0530

    docs: add code comments

--- core.py
+++ core.py
@@ -1,3 +1,6 @@
+'''
+This module contains all the necessary custom built functions required
+'''
 import File_handle
 import pypdfium2 as pdfium
 from PIL import Image
@@ -5,7 +8,10 @@ import shutil
 
 pgs = 1
 
-
+'''
+This function extracts all the pages in a given pdf file and stores
+them as images in subfolder 'images' in the parent directory
+'''
 def file_to_img(file_path):
     global pgs
 
@@ -19,14 +25,19 @@ def file_to_img(file_path):
 
     pdf.close()
 
-
+'''
+This function merges all saved images into a new pdf file
+'''
 def imgs_to_file():
     global pgs
 
     load = lambda x: Image.open(r"images\page_"+str(x)+".jpg")
     Image.open(r"images\page_0.jpg").save("output.pdf", save_all=True, append_images=[load(j) for j in range(1, pgs)])
 
-
+'''
+This function is used to clear the folder containing the 
+individual image versions of all the pages
+'''
 def clean():
     shutil.rmtree("images")
     File_handle.create_path("images")
